352e336d5ec5c0486960aee12f355145237fe796
mariobarrosmbn <166873448+mariobarrosmbn@users.noreply.github.com>
2025-01-02 08:37:17 -0300

    add gemfile and rubocop files

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,21 +1,21 @@
+# frozen_string_literal: false
+
 def caesar_cipher(string, shift)
-  encrypted_string = ""
-  arr = string.split("")
+  encrypted_string = ''
+  arr = string.chars
   arr.map do |char|
     if char.ord.between?(65, 90)
-      num_char = char.ord + shift % 26
-      if num_char > 90
-        num_char = (char.ord + shift % 26) - 26
-      end
+      num_char = char.ord + (shift % 26)
+      num_char = (char.ord + (shift % 26)) - 26 if num_char > 90
     elsif char.ord.between?(97, 122)
-      num_char = char.ord + shift % 26
-      if num_char > 122
-        num_char = (char.ord + shift % 26) - 26
-      end
+      num_char = char.ord + (shift % 26)
+      num_char = (char.ord + (shift % 26)) - 26 if num_char > 122
     else
       num_char = char
     end
     encrypted_string << num_char
-  end    
+  end
   puts encrypted_string
 end
+
+caesar_cipher('mario', 15)
